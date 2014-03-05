91f4871f4574351a14216f76842bf60cf9652e33
Joe Stein <joe.stein@stealth.ly>
2014-03-04 23:38:46 -0500

    help to sign and make builds

---
+++ scripts/verify.sh
@@ -0,0 +1,38 @@
+# Licensed to the Apache Software Foundation (ASF) under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+# 
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+#!/bin/sh -Eux
+
+#  Trap non-normal exit signals: 1/HUP, 2/INT, 3/QUIT, 15/TERM, ERR
+trap founderror 1 2 3 15 ERR
+
+founderror()
+{
+        exit 1
+}
+
+exitscript()
+{
+        #remove lock file
+        #rm $lockfile
+        exit 0
+}
+
+gpg --verify kafka-0.8.1-src.tgz.asc kafka-0.8.1-src.tgz
+
+/vagrant/scripts/verify_hash.sh kafka-0.8.1-src.tgz MD5 md5
+/vagrant/scripts/verify_hash.sh kafka-0.8.1-src.tgz SHA1 sh1
+/vagrant/scripts/verify_hash.sh kafka-0.8.1-src.tgz SHA256 sh2
+
